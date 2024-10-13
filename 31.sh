ae36fbb32f89e9bc8338ef4bd58f6a5a5de3802c
boosterka <ebrou833@gmail.com>
2024-10-14 02:06:42 +0400

    Update 31.sh

--- 31.sh
+++ 31.sh
@@ -1,3 +1,3 @@
-sed -i 's/^INTERFACESv4=""/INTERFACESv4="eth0"/' /etc/default/isc-dhcp-server
+sed -i 's/^INTERFACESv4=""/INTERFACESv4="eth1"/' /etc/default/isc-dhcp-server
 
-sed -i 's/^INTERFACESv6=""/INTERFACESv6=""/' /etc/default/isc-dhcp-server
+sed -i 's/^INTERFACESv6=""/INTERFACESv6="eth1"/' /etc/default/isc-dhcp-server
