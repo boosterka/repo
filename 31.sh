43c30e130c4b596e64d446273d4e5864cad5b022
boosterka <ebrou833@gmail.com>
2024-10-15 00:51:19 +0400

    Update 31.sh

--- 31.sh
+++ 31.sh
@@ -1,4 +1,4 @@
-apt install isc-dhcp-server -y
+
 sed -i 's/^INTERFACESv4=""/INTERFACESv4="eth1"/' /etc/default/isc-dhcp-server
 
 sed -i 's/^INTERFACESv6=""/INTERFACESv6="eth1"/' /etc/default/isc-dhcp-server
